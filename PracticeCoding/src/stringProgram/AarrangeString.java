4ebbe5b8afe9964a2e70aae1b3149c9b3be84621
Anil <itanilkumar05@gmail.com>
2022-06-20 10:36:38 +0530

    added some string questions

--- PracticeCoding/src/stringProgram/AarrangeString.java
+++ PracticeCoding/src/stringProgram/AarrangeString.java
@@ -3,19 +3,21 @@ package stringProgram;
 public class AarrangeString {
 
 	public static void main(String[] args) {
-		String a ="",b="";
-		String s ="anil 955 dash";
+		
+		String s="anil d 955";
+		String a=" ",b=" ";
 		for(int i=0;i<s.length();i++) {
-			char c =s.charAt(i);
+			char c=s.charAt(i);
 			if(Character.isDigit(c)) {
 				a=a+c;
-				
 			}else {
 				b=b+c;
 			}
 		}
+		
 		System.out.println(a.concat(b));
-
+		
+		
 	}
-
+		
 }
--- PracticeCoding/src/stringProgram/LongestWord.java
+++ PracticeCoding/src/stringProgram/LongestWord.java
@@ -1,18 +1,23 @@
 package stringProgram;
 
 public class LongestWord {
-
 	public static void main(String[] args) {
-		
-		String s = "ani kumar";
+		String s="anil kumar dashsdf";
+		String w="";s+=" "; String lw=" ";
 		for(int i=0;i<s.length();i++) {
-			if(i%2==0) {
-			int a =s.indexOf(i);
+			char c=s.charAt(i);
+			if(c!=' ') {
+				w=w+c;
+			}else {
+				
+				if(w.length()>lw.length()) {
+					lw=w;
+					w="";
+				}
+			}
 			
-			
-		}
-			
-
-	}}
+		}System.out.println(lw);
+		
+	}
 
 }
